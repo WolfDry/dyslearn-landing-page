8fb2a47bba02ceef1f908317d01d87d4446e6df4
Benjamin <benjamindeltour22@gmail.com>
2023-12-06 16:41:27 +0100

    add: header finished

---
+++ src/Home/Home.js
@@ -0,0 +1,25 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="header">
+        <a href="#">Qui somme nous ?</a>
+        <a href="#">Notre solution</a>
+        <a href="#">Processus</a>
+        <a href="#">Nos avantages</a>
+        <a href="#">L'application</a>
+        <a className='primary-button' href="#">Nous contacter</a>
+      </header>
+      <div className="logo-container">
+        <img src="./Logo.png"/>
+        <div className='logo-bottom'/>
+      </div>
+      <section>
+        
+      </section>
+    </div>
+  );
+}
+
+export default Home;

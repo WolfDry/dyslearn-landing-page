d23da935122ca65343aa8a70c542be0c8c6d1eb4
Benjamin <benjamindeltour22@gmail.com>
2023-12-07 16:38:29 +0100

    landing page over

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,22 +2,244 @@ import './Home.css'
 
 function Home() {
   return (
-    <div className="App">
-      <header className="header">
-        <a href="#">Qui somme nous ?</a>
-        <a href="#">Notre solution</a>
-        <a href="#">Processus</a>
-        <a href="#">Nos avantages</a>
-        <a href="#">L'application</a>
-        <a className='primary-button' href="#">Nous contacter</a>
-      </header>
-      <div className="logo-container">
-        <img src="./Logo.png"/>
-        <div className='logo-bottom'/>
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <div className="qui-sommes-nous">
+            <img className="robo-wunderkind" alt="tablette app" src="./img/tablet-app.png" />
+          </div>
+          <div className="rectangle" />
+          <div className="vous-tes-spcialistes">Vous êtes spécialistes des troubles de l'apprentissage</div>
+          <p class="dyslearn-allie">DysLearn est l'allié incontournable des professionnels de l'éducation confrontés aux troubles de l'apprentissage. Avec une approche personnalisée, notre application offre des parcours adaptés et un suivi précis des progrès. Rejoignez-nous pour faire de l'inclusion éducative une réalité quotidienne dans votre cabinet. Fournissez une éducation qui transcende les obstacles et ouvre des portes pour chaque apprenant. DysLearn, la clé de l'apprentissage inclusif.</p>
+          <img className="fichier" alt="Fichier" src="./img/number-two-yellow.png" />
+          <img className="fichier-x" alt="Fichier" src="./img/number-nine.png" />
+        </div>
+        <div className="text-wrapper">Les avantages de DysLearn</div>
+        <div className="overlap-group">
+          <div className="overlap-2">
+            <div className="overlap-3">
+              <p className="p">Une interface propice à l'apprentissage académique de nos enfants</p>
+              <img className="img" alt="Fichier" src="./img/number-three.png" />
+            </div>
+            <div className="overlap-4">
+              <div className="text-wrapper-2">Jeux</div>
+              <div className="line" />
+              <img className="capture-decran" alt="Capture decran" src="./img/app-screen.png" />
+            </div>
+            <div className="overlap-5">
+              <div className="text-wrapper-3">Espace compte</div>
+              <div className="line-2" />
+            </div>
+            <img className="fichier-2" alt="Fichier" src="./img/equal-pink.png" />
+          </div>
+          <div className="overlap-6">
+            <div className="text-wrapper-4">Leçons</div>
+            <div className="line-3" />
+          </div>
+          <div className="overlap-7">
+            <div className="text-wrapper-4">Récompense</div>
+            <div className="line-4" />
+          </div>
+        </div>
+        <div className="text-wrapper-5">Autonomie</div>
+        <div className="text-wrapper-6">Adaptabilité</div>
+        <div className="text-wrapper-7">Innovation</div>
+        <div className="overlap-8">
+          <div className="overlap-9">
+            <p className="faire-de-nos-enfants">
+              Faire de nos enfants <br />
+              les héros des chiffres !
+            </p>
+            <div className="overlap-10">
+              <div className="rectangle-2">
+                <p className='link'>
+                  Qui sommes nous ?
+                </p>
+                <p className='link'>
+                  Notre solution
+                </p>
+                <p className='link'>
+                  Nos avantages
+                </p>
+                <p className='link'>
+                  L'application
+                </p>
+              </div>
+              <div className="CTA-decouvrir">
+                <div className="div-wrapper">
+                  <div className="text-wrapper-8">Nous contacter</div>
+                </div>
+              </div>
+              <div className="rectangle-3" />
+              <img className="logo-bleu-dys" alt="Logo" src="./Logo.png" />
+            </div>
+            <div className="overlap-11">
+              <img className="mask-group" alt="Mask group" src="./img/kid.jpeg" />
+              <img className="bh" alt="perso kid" src="./img/perso-kid.png" />
+              <img className="BH" alt="perso read" src="./img/perso-read.png" />
+            </div>
+            <div className="overlap-12">
+              <img className="BH-x" alt="perso fall" src="./img/perso-fall.png" />
+              <div className="sont-touch-s-par-la">
+                Sont touchés par la dyscalculie
+              </div>
+              <div className="flexcontainer">
+                <p className="text">
+                  <span className="span">
+                    Chiffres clés <br /> de la dyscalculie chez nos enfants
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div className="overlap-13">
+              <div className="text-wrapper-9">4%</div>
+            </div>
+            <div className="overlap-14">
+              <div className="rectangle-4" />
+              <div className="text-wrapper-10">60%</div>
+            </div>
+            <div className="overlap-15">
+              <div className="rectangle-5" />
+              <div className="rectangle-6" />
+              <div className="text-wrapper-11">Bienvenue sur DysLearn</div>
+              <div className="flexcontainer-2">
+                <p className="span-wrapper">
+                  <span className="text-wrapper-12">
+                    Une initiative passionnante née de la collaboration entre des étudiants de My Digital School. Nous sommes une équipe dévouée, dont les fondements reposent sur la conviction que chaque enfant, quels que soient ses défis, mérite une éducation exceptionnelle.
+                  </span>
+                </p>
+              </div>
+              <div className="text-wrapper-13">75%</div>
+              <img className="fichier-3" alt="Fichier" src="./img/perso-discuss-plus.png" />
+              <img className="fichier-4" alt="Fichier" src="./img/perso-discuss.png" />
+              <img className="fichier-5" alt="Fichier" src="./img/number-zero.png" />
+              <img className="fichier-6" alt="Fichier" src="./img/number-five.png" />
+            </div>
+          </div>
+          <p className="surmonter-la">
+            Surmonter la dyscalculie <br />
+            et conquérir les chiffres.
+          </p>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group-2">
+              <div className="text-wrapper-14">Découvrir</div>
+            </div>
+          </div>
+          <img className="BH-2" alt="perso fall" src="./img/perso-fall.png" />
+          <div className="rencontrent-des">
+            Rencontrent des <br />
+            difficultés académiques
+          </div>
+          <p className="ont-des-probl-mes-d">
+            Ont des problèmes <br />
+            d'estime de soi
+          </p>
+        </div>
+        <p className="dyslearn-est-bien">
+          DysLearn est bien plus qu&#39;une simple application éducative, c&#39;est une aventure d&#39;apprentissage
+          immersive conçue pour aider les enfants à surmonter les défis de la dyslexie et de la dyscalculie tout en
+          s&#39;amusant.
+        </p>
+        <div className="text-wrapper-15">La Solution DysLearn</div>
+        <div className="overlap-16">
+          <div className="overlap-17">
+            <div className="overlap-18">
+              <div className="rectangle-7" />
+              <div className="rectangle-8" />
+              <div className="rectangle-9" />
+              <p className="dyslearn-offre-aux">
+                DysLearn offre aux enfants la possibilité d&#39;explorer des mondes enchanteurs, résolvant des énigmes.
+                Chaque étape renforce les compétences tout en stimulant la créativité.
+              </p>
+              <div className="text-wrapper-16">Explorer</div>
+              <div className="text-wrapper-17">Grandir</div>
+              <div className="text-wrapper-18">Apprendre</div>
+              <div className="text-wrapper-19">Une aventure éducative interactive</div>
+              <div className="un-parcours">Un parcours personnalisé</div>
+              <div className="m-thodes-p">
+                Méthodes <br />
+                pédagogiques
+              </div>
+              <p className="dyslearn-n-est-pas">
+                DysLearn n&#39;est pas juste une application, c&#39;est un compagnon de croissance. Chaque enfant
+                progresse à son rythme, construisant un parcours éducatif qui lui est propre.
+              </p>
+              <p className="dyslearn-utilise-une">
+                DysLearn utilise une approche adaptative, ajustant les activités en fonction des besoins spécifiques de
+                chaque enfant. Chaque activité est conçue pour maximiser l&#39;apprentissage.
+              </p>
+            </div>
+            <img className="fichier-7" alt="Fichier" src="./img/plus.png" />
+          </div>
+          <div className="line-5" />
+          <img className="fichier-8" alt="Fichier" src="./img/number-two.png" />
+        </div>
+        <div className="overlap-19">
+          <div className="overlap-wrapper">
+          </div>
+          <div className="CTA-decouvrir-2">
+            <div className="overlap-20">
+              <div className="text-wrapper-14">Devenir BTA</div>
+            </div>
+          </div>
+          <svg width="1375" height="621" viewBox="0 0 1375 621" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M-14 4L1224.57 0.43321C1307.58 0.194157 1375 67.4211 1375 150.433V502.431C1375 585.105 1308.11 652.193 1225.43 652.431L-14 656V4Z" fill="#023436"/>
+          </svg> 
+          <div class="text-wrapper-21">Deviens l'un des <span>premiers</span> à tester notre solutions:</div>
+          <svg className="vector" width="371" height="95" viewBox="0 0 371 95" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.00001 27C2.34615 5.76908 240.769 4.99999 267.667 4.99999C282.864 4.99999 298.511 3.84042 313.444 6.55554C327.14 9.04557 339.568 5.0301 353 11C355.712 12.2054 371.321 15 365 15C358.185 15 351.37 15 344.556 15C317.786 15 287.957 18.2485 262.778 27.4444C246.929 33.2327 231.007 35.9966 214.778 40.1111C206.654 42.1705 200.117 45 212.778 45C230.062 45 247.312 42.7897 264.556 43C272.824 43.1008 272.514 45.4675 269.444 53.2222C267.334 58.5539 263.485 66.2821 261.889 71.8889C248.832 117.765 265.703 57.3799 263 79" stroke="#FCBD25" stroke-width="9" stroke-linecap="round"/>
+          </svg>
+          <input type='text' className='inputFirstName input' placeholder='Prénom' />
+          <input type='text' className='inputName input' placeholder='Nom' />
+          <input type='email' className='inputEmail input' placeholder='Email' />
+          <input type='password' className='inputPassword input' placeholder='Mot de passe' />
+          <img className="bh-x" alt="perso kid" src="./img/perso-kid.png" />
+        </div>
+        <div className="text-wrapper-20">+</div>
+        <img className="rectangle-10" alt="Rectangle" src="rectangle-7.svg" />
+        <div className="line-6"/>
+        <div className="overlap-21">
+          <img className="fichier-9" alt="Fichier" src="./img/perso-dance.png" />
+          <svg className="vector-2" width="16" height="41" viewBox="0 0 16 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2 2C9.35797 2.45987 14 16.4504 14 22.4444C14 27.5136 13.7521 30.2304 11.0556 34.5C10.0874 36.033 8 39 8 39" stroke="black" stroke-width="4" stroke-linecap="round"/>
+          </svg>
+          <svg className="vector-3" width="16" height="33" viewBox="0 0 16 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2 2C6.54005 2.80118 9.98291 12.9353 11.8889 16.6111C13.7551 20.2103 13.7997 27.4007 12 31" stroke="black" stroke-width="4" stroke-linecap="round"/>
+          </svg> 
+        </div>
+        <div className="overlap-22">
+          <img className="fichier-15" alt="Fichier" src="./img/perso-fall-pink.png" />
+          <svg className="vector-4" width="45" height="33" viewBox="0 0 45 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2 31C20.3911 31 32.4786 15.1518 43 2" stroke="black" stroke-width="4" stroke-linecap="round"/>
+          </svg>
+          <svg className="vector-5" width="29" height="23" viewBox="0 0 29 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2 21C5.84639 17.5382 11.195 15.4455 15.5 12.5C19.602 9.69335 23.9269 5.84131 27 2" stroke="black" stroke-width="4" stroke-linecap="round"/>
+          </svg> 
+        </div>
+        <div className="overlap-23">
+          <img className="fichier-9" alt="Fichier" src="./img/perso-read-green.png" />
+          <svg className="vector-6" width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3.99982 6.99985C-0.170834 6.73919 6.47462 2.54673 7.99982 2.11096C12.3557 0.866415 12.0869 10.7847 11.9998 13.2221C11.8529 17.3358 14.4897 15.9999 18.4443 15.9999C20.4604 15.9999 21.7056 16.2327 21.9443 18.4999C22.0664 19.6603 21.9998 21.9999 21.9998 21.9999" stroke="black" stroke-width="4" stroke-linecap="round"/>
+          </svg>
+          <svg className="vector-7" width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2 3.00007C15.3549 -1.10912 3.29091 12.0001 11.2222 12.0001C14.8174 12.0001 17.7734 11.4688 18 16.0001C18.1053 18.1056 16 22.0001 16 22.0001" stroke="black" stroke-width="4" stroke-linecap="round"/>
+          </svg> 
+        </div>
+        <img className="fichier-10" alt="Fichier" src="./img/divide.png" />
+        <img className="fichier-11" alt="Fichier" src="./img/equal.png" />
+        <img className="fichier-12" alt="Fichier" src="./img/number-one.png" />
+        <img className="fichier-13" alt="Fichier" src="./img/black-divide.png" />
+        <img className="logo-bleu-dys-2" alt="Logo bleu dys" src="./Logo.png" />
+        <p className='address'>
+          26 rue villette 69003, LYON
+        </p>
+        <p className='copyright'>
+          @Copyright
+        </p>
       </div>
-      <section>
-        
-      </section>
+      <div>
+        <img className="chat-boot" alt="Chat boot" src="./img/chat-bot.png" />
+      </div>
     </div>
   );
 }
